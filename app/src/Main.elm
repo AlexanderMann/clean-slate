5e565544005c66698efec4c548b9f5ef81202159
Travis Shears <btbtravis@gmail.com>
2018-04-13 13:16:04 -0500

    Stand up basic interactive flow

--- app/src/Main.elm
+++ app/src/Main.elm
@@ -1,21 +1,60 @@
 module Main exposing (..)
 
+--import Json.Decode exposing (..)
+--import Debug exposing (log)
+
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
 
 
+--import Array exposing (..)
 --import Html exposing (Html, text, div, h1, img)
 --import Html.Attributes exposing (src, class)
 ---- MODEL ----
 
 
+type alias Node =
+    { title : String
+    , id : String
+    , children : List String
+    , nodeType : String
+    , info : String
+    }
+
+
 type alias Model =
-    {}
+    { currentNodeId : String
+    , nodes : List Node
+    }
 
 
-init : ( Model, Cmd Msg )
-init =
-    ( {}, Cmd.none )
+type alias Flags =
+    { root : String
+    , nodes : List Node
+    }
+
+
+initialModel : Model
+initialModel =
+    { currentNodeId = ""
+    , nodes = []
+    }
+
+
+init : Flags -> ( Model, Cmd Msg )
+init flags =
+    ( { initialModel
+        | currentNodeId = flags.root
+        , nodes = flags.nodes
+      }
+    , Cmd.none
+    )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
 
 
 
@@ -24,22 +63,33 @@ init =
 
 type Msg
     = NoOp
+    | Answer String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        Answer x ->
+            ( { model | currentNodeId = x }, Cmd.none )
+
+        NoOp ->
+            ( model, Cmd.none )
 
 
 
 ---- VIEW ----
 
 
+getNode : String -> List Node -> Maybe Node
+getNode id nodes =
+    List.filter (\x -> x.id == id) nodes |> List.head
+
+
 view : Model -> Html Msg
 view model =
     div []
         [ div [ id "topbar" ]
-            [ h1 [] [ text "prison-help" ] ]
+            [ h1 [] [ text ("prison-help") ] ]
         , div [ class "wrapper" ]
             [ div [ id "results" ]
                 [ div [ class "title" ] [ text "RESULTS" ]
@@ -47,29 +97,72 @@ view model =
                 ]
             , div [ id "questions" ]
                 [ div [ class "title" ] [ text "QUESTIONS" ]
-                , div [ class "card" ]
-                    [ p [ class "question" ] [ text "Can the case be reduced by a recent Proposition?" ]
-                    , div [ class "hr" ] []
-                    , div [ class "info" ] [ text "extra info" ]
-                    , div [ class "options" ]
-                        [ div [ class "option" ] [ text "YES" ]
-                        , div [ class "option" ] [ text "NO" ]
-                        ]
-                    ]
+                , card model.currentNodeId model.nodes
                 ]
             ]
         ]
+
+
+card : String -> List Node -> Html Msg
+card id nodes =
+    let
+        node =
+            case getNode id nodes of
+                Nothing ->
+                    { title = "Missing Node"
+                    , id = "0"
+                    , children = []
+                    , nodeType = "none"
+                    , info = ""
+                    }
+
+                Just x ->
+                    x
+
+        options =
+            if (.id node) /= "0" then
+                -- <function> : List String -> Maybe Node
+                List.map (\x -> ( x, nodes )) (.children node)
+                    |> List.map (\x -> getNode (Tuple.first x) (Tuple.second x))
+                    |> List.map
+                        (\x ->
+                            case x of
+                                Just y ->
+                                    List.head (.children y)
+                                        |> (\z ->
+                                                case z of
+                                                    Just a ->
+                                                        a
+
+                                                    Nothing ->
+                                                        "1"
+                                           )
+                                        |> (\b -> div [ class "option", onClick (Answer b) ] [ text (.title y) ])
+
+                                Nothing ->
+                                    div [] []
+                        )
+            else
+                [ p [] [ text "End of line plase see results" ] ]
+    in
+        div [ class "card" ]
+            [ p [ class "question" ] [ text (.title node) ]
+            , div [ class "hr" ] []
+            , div [ class "info" ] [ text (.info node) ]
+            , div [ class "options" ]
+                options
+            ]
 
 
 
 ---- PROGRAM ----
 
 
-main : Program Never Model Msg
+main : Program Flags Model Msg
 main =
-    Html.program
+    Html.programWithFlags
         { view = view
         , init = init
         , update = update
-        , subscriptions = always Sub.none
+        , subscriptions = subscriptions
         }
--- app/src/index.js
+++ app/src/index.js
@@ -1,7 +1,23 @@
 import './main.css';
 import { Main } from './Main.elm';
 import registerServiceWorker from './registerServiceWorker';
+import data from '../../data/flow.json'
 
-Main.embed(document.getElementById('root'));
+console.log('data: ', data);
+// next keys and turn nodes into an flat array
+const nodes = Object.keys(data.nodes).map(key => {
+  let node = data.nodes[key];
+  node.nodeType = node.type; // type is protected key in elm so node.type == node.nodeType
+  delete node.type;
+  node.info = node.text;
+  delete node.text;
+  node.id = key;
+  return node;
+});
+console.log('nodes: ', nodes);
+Main.embed(document.getElementById('root'), {
+  root: data.root,
+  nodes: nodes
+});
 
 registerServiceWorker();
